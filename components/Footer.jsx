4789618039651153dfb09d65bc9d94c3d347abab
Aabid Ali <mdaabidali28@gmail.com>
2025-03-06 22:41:18 +0530

    header and footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className=' py-16 text-center bg-blue-100 text-gray-600'>
+            <p>developed by aabid ali</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
