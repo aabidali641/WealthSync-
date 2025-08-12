d893881685265f47f2c848b31dadaa95d4c48135
Aabid Ali <mdaabidali28@gmail.com>
2025-08-12 14:48:03 +0530

    Package Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-
-
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/api/portraits/**",
-      },
-    ],
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
-
